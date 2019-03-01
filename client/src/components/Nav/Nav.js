c5a1b1a4905cb29273efac325b68bad5227da628
kloperena <kay.loperena@gmail.com>
2019-02-28 19:03:55 -0500

    kayla

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,9 +2,62 @@
 import React from "react";
 import firebase from "firebase";
 import "./style.css";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+//import IconButton from '@material-ui/core/IconButton';
+//import MenuIcon from '@material-ui/icons/Menu';
+import ImageAvatars from "../avimg/avatars";
+import TextButtons from "../navButton/navButton";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function  Nav(props) {
-  return <div className="Nav">{props.children}</div>;
+
+ const styles = {
+   root: {
+    flexGrow: 1,
+   },
+   grow: {
+     flexGrow: 1,
+   },
+   menuButton: {
+     marginLeft: -12,
+     marginRight: 20,
+   },
+
+ };
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="display1" color="inherit" className={classes.grow} >
+            Memory Game
+          </Typography>
+          <ImageAvatars> 
+
+          </ImageAvatars>
+          <div className= "signOut">
+       <TextButtons><a href="/">Go back</a></TextButtons>
+          
+            {/* <button  onClick={() => firebase.auth().signOut()}>Sign out!</button> */}
+
+            {/* <Button onClick={() =>Route exact path="/" component={Home}}  </Button> */}
+            <TextButtons  onClick={() => firebase.auth().signOut()}>Sign out!</TextButtons>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Nav;
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
